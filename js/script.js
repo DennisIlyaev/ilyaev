a3d140eb9fa7409d93d399dae456f4e76624a7c4
DennisIlyaev <DennisIlyaev@yahoo.com>
2015-05-31 18:10:33 +0300

    init files

---
+++ js/script.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+
+// Adding/Removing active class on click in mainNavigation.
+
+$('#mainNav').on('click', 'li' ,function() {
+	$('li').removeClass('active');
+	$(this).addClass('active');
+
+ });
+
+
+//  Loading Pictures page.
+
+$('#picNav').on('click', function() {
+	$('#memPage').addClass('hiddenPage');
+	$('#conPage').addClass('hiddenPage');
+	$('#vidPage').addClass('hiddenPage');
+	$('#homePage').addClass('hiddenPage');
+	$('#picPage').removeClass('hiddenPage');
+});
+
+// 	Loading Home page.
+
+$('#homeNav').on('click', function() {
+	$('#picPage').addClass('hiddenPage');
+	$('#memPage').addClass('hiddenPage');
+	$('#conPage').addClass('hiddenPage');
+	$('#vidPage').addClass('hiddenPage');
+	$('#homePage').removeClass('hiddenPage');
+});	
+
+// 	Loading Videos page.
+
+$('#vidNav').on('click', function() {
+	$('#picPage').addClass('hiddenPage');
+	$('#memPage').addClass('hiddenPage');
+	$('#conPage').addClass('hiddenPage');
+	$('#homePage').addClass('hiddenPage');
+	$('#vidPage').removeClass('hiddenPage');
+});
+
+// 	Loading Members page.
+
+$('#memNav').on('click', function() {
+	$('#picPage').addClass('hiddenPage');
+	$('#homePage').addClass('hiddenPage');
+	$('#vidPage').addClass('hiddenPage');
+	$('#conPage').addClass('hiddenPage');
+	$('#memPage').removeClass('hiddenPage');
+});
+
+// 	Loading contact page.
+
+$('#conNav').on('click', function() {
+	$('#picPage').addClass('hiddenPage');
+	$('#memPage').addClass('hiddenPage');
+	$('#homePage').addClass('hiddenPage');
+	$('#vidPage').addClass('hiddenPage');
+	$('#conPage').removeClass('hiddenPage');
+});
+
+// Preventing page jump on anchors.
+
+$('#upcomingEvents, nav, footer').on('click', 'a', function(e){
+	e.preventDefault();
+});
+
+
+
+
+
+})();
